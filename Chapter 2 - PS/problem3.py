cb3fd2049f3985464d835a977f6944d625f10c67
Saif Ul Islam <saifsafi2807@gmail.com>
2026-02-24 23:22:09 +0500

    Chapter 2 done

---
+++ Chapter 2 - PS/problem3.py
@@ -0,0 +1,6 @@
+
+a = int(input("Enter a number: "))
+
+print("You entered:", a)
+
+print("Type is: ", type(a))
---
+++ Chapter 2/02_datatypes.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer
+
+b = 4.22 # b is a floating point numbers
+
+c = "Saif" # c is a string
+
+d = True # d is a boolean variable
+
+e = None # e is a none type variable
