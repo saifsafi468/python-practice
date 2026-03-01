71e88f42ac98ed8484a41a38b707bd342bd956a8
Saif Ul Islam <saifsafi2807@gmail.com>
2026-03-01 22:28:10 +0500

    Chapter 6 - PS done

---
+++ Chapter 6 - PS/Problem3.py
@@ -0,0 +1,9 @@
+comment = input("Enter comment: ")
+
+if ("make a lot of money" in comment or
+    "buy now" in comment or
+    "subscribe this" in comment or
+    "click this" in comment):
+    print("Spam detected")
+else:
+    print("Not spam")
---
+++ Chapter 6 - PS/Problem7.py
@@ -0,0 +1,6 @@
+post = input("Enter post: ")
+
+if "saif" in post.lower():
+    print("This post is talking about Saif")
+else:
+    print("Not about Saif")
