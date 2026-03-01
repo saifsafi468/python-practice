cc679b8be5e789f3ff49f909e0f49f0be44bbef2
Saif Ul Islam <saifsafi2807@gmail.com>
2026-03-01 22:40:39 +0500

    Chapter 7 done

---
+++ Chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,18 @@
+
+## for loop with lists
+#l = [1, 342, 456, 25, 9]
+#
+#for i in l:
+#    print(i)
+
+
+# for loop with tuple
+t = (1, 45, 32, 66)
+
+for i in t:
+    print(i)
+
+## for loop with string
+s = "Saif"
+for i in s:
+    print(i)
